bf0fe6b543a365af518c5ca3035d62ba365068f6
Gaganyadav75 <7549gaganyadav@gmail.com>
2024-12-03 22:21:22 +0530

    main updated

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 int main()
-{
-    printf("hello world");
+{int inp;
+
+    do {
+
+        printf("\n ## 0 to exit ##");
+        printf("\nEnter Program No. 1 - 10 : ");
+        scanf("%d", &inp);
+
+        system("cls");
+
+        if (inp != 0) {
+            Assignment1(inp);
+        }
+
+    } while (inp != 0); 
 return 0;
 }
 
